460da0357f6a60fc6f92778a7a2e86fe79f3716b
Ryan Case <rmcase@gmail.com>
2015-02-04 10:29:37 -0700

    Moved the node modules to the top level, added code for clustered processes

---
+++ cluster/zmq-file-loop-req.js
@@ -0,0 +1,26 @@
+#! /usr/bin/env node --harmony
+
+'use strict';
+
+const
+  zmq = require('zmq'),
+  filename = process.argv[2],
+
+  // Create request endpoint
+  requester = zmq.socket('req');
+
+// Handle replies
+requester.on('message', function(data) {
+  let response = JSON.parse(data);
+  console.log("Received response: ", response);
+});
+
+requester.connect("tcp://localhost:5433");
+
+// Send content request
+for (let i = 0; i < 5; i++) {
+  console.log('Sending request for ' + filename);
+  requester.send(JSON.stringify({
+    path: filename
+  }));
+}
